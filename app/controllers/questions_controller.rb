aa123e531bc0143d4188b52c806aff0eefad76dd
vernicode <konstantinvernigorov.kot03@gmail.com>
2022-09-23 11:17:01 +0300

    v0.5

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,44 @@
 class QuestionsController < ApplicationController
-    skip_before_action :verify_authenticity_token
+    before_action :set_question, only: %i[update show destroy edit]
     
     def create
-        Question.create(
-            body: params[:question][:body],
-            user_id: params[:question][:user_id]
-        )
+        question = Question.create(question_params)
+
+        redirect_to question_path(question)
     end
     
     def update
-        @question = Question.find(params[:id])
-        @question.update(
-            body: params[:question][:body],
-            user_id: params[:question][:user_id]
-        )
+        @question.update(question_params)
+
+        redirect_to question_path
     end
 
     def destroy
-        @question = Question.find(params[:id])
         @question.destroy
+
+        redirect_to questions_path
     end
+    
+    def show
+    end
+
+    def index 
+        @questions = Question.all
+    end
+
+    def new
+        @question = Question.new
+    end
+
+    def edit
+    end
+
+    private
+        def question_params
+            params.require(:question).permit(:body, :user_id)
+        end
+
+        def set_question
+            @question = Question.find(params[:id])
+        end
 end
