dbf4154c9891bd70437f1e10168fb1b1e210b542
vernicode <konstantinvernigorov.kot03@gmail.com>
2022-09-25 18:16:53 +0300

    v0.7

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,19 +4,21 @@ class QuestionsController < ApplicationController
     def create
         question = Question.create(question_params)
 
-        redirect_to question_path(question)
+        # flash[:notice] = 'Новый вопрос создан!'
+
+        redirect_to question_path(question), notice: 'Новый вопрос создан!'
     end
     
     def update
         @question.update(question_params)
 
-        redirect_to question_path
+        redirect_to question_path, notice: 'Сохранили вопрос!'
     end
 
     def destroy
         @question.destroy
 
-        redirect_to questions_path
+        redirect_to questions_path, notice: 'Вопрос удалён'
     end
     
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
   resources :questions
+  resources :users, only: %i[new create]
 
   # post '/questions', to: 'questions#create'
   # patch '/questions/:id', to: 'questions#update'
